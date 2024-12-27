01e941b48139513d9b58970b0b8b2eba31eaca42
1winhyun <jimking1@naver.com>
2024-12-27 11:59:37 +0900

    feat: 주석 추가

--- src/main/java/com/example/jwt_exercise/user/config/jwt/JwtProperties.java
+++ src/main/java/com/example/jwt_exercise/user/config/jwt/JwtProperties.java
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Component;
 @Component
 @Getter
 @Setter
-@ConfigurationProperties("jwt")
+@ConfigurationProperties("jwt")//자바 클래스에 프로퍼티 값을 가져와서 사용하는 어노테이션
 public class JwtProperties {
-    private String issuer;
-    private String secretKey;
+    private String issuer;//토큰 발급자
+    private String secretKey;//서명, 비밀값
 }
